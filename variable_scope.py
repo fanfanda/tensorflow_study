dd10433b9a7e53c211e5e8655177e1eb9fbda932
fanfanda <fanfanda@ict.ac.cn>
2018-03-04 22:22:17 +0800

    nothing

---
+++ variable_scope.py
@@ -0,0 +1,90 @@
+# import tensorflow as tf
+# from tensorflow.examples.tutorials.mnist import input_data
+
+# # import matplotlib.pyplot as plt
+# # import sys
+
+
+# # print(mnist.train.num_examples)
+
+# INPUT_NODE = 784
+# OUTPUT_NODE = 10
+
+# LAYER1_NODE = 500
+# BATCH_SIZE = 100
+
+# LEARNING_RATE_BASE = 0.8
+# LEARNING_RATE_DECAY = 0.99
+
+# REGULARIZATION_RATE = 0.0001
+# TRAINING_STEPS = 30000
+# MOVING_AVERAGE_DECAY = 0.99 #滑动平均衰减
+
+# def inference(input_tensor, reuse = False):
+#     with tf.variable_scope('layer1', reuse = reuse):
+#         weights = tf.get_variable("weights", [INPUT_NODE, LAYER1_NODE], initializer=tf.truncated_normal_initializer(stddev=0.1))
+#         biases = tf.get_variable("biases", [LAYER1_NODE], initializer=tf.constant_initializer(0.0))
+#         layer1 = tf.nn.relu(tf.matmul(input_tensor, weights)+biases)
+#     with tf.variable_scope('layer2', reuse = reuse):
+#         weights = tf.get_variable("weights", [LAYER1_NODE, OUTPUT_NODE], initializer=tf.truncated_normal_initializer(stddev=0.1))
+#         biases = tf.get_variable("biases", [OUTPUT_NODE], initializer=tf.constant_initializer(0.0))
+#         layer2 = tf.matmul(layer1, weights)+biases
+#     return layer2
+
+# def train(mnist):
+#     x = tf.placeholder(tf.float32,shape=(None, INPUT_NODE),name='x-input')
+#     y_ = tf.placeholder(tf.float32,shape=(None, OUTPUT_NODE),name='y-input')
+
+#     weights1 = tf.Variable(tf.truncated_normal([INPUT_NODE, LAYER1_NODE],stddev=0.1))
+#     biases1 = tf.Variable(tf.constant(0.1, shape=[LAYER1_NODE]))
+
+#     weights2 = tf.Variable(tf.truncated_normal([LAYER1_NODE, OUTPUT_NODE],stddev=0.1))
+#     biases2 = tf.Variable(tf.constant(0.1, shape=[OUTPUT_NODE]))
+
+#     y = inference(x)
+#     global_step = tf.Variable(0, trainable=False)
+
+#     variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+#     variable_averages_op = variable_averages.apply(tf.trainable_variables())
+
+#     # average_y = inference(x,variable_averages, weights1, biases1, weights2, biases2)
+
+#     cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1))
+#     cross_entropy_mean = tf.reduce_mean(cross_entropy)
+
+#     regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
+#     regularization = regularizer(weights1) + regularizer(weights2)
+
+#     loss = cross_entropy_mean + regularization
+#     learning_rate = tf.train.exponential_decay(LEARNING_RATE_BASE, global_step, mnist.train.num_examples / BATCH_SIZE, LEARNING_RATE_DECAY)
+#     train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+
+#     train_op = tf.group(train_step, variable_averages_op)
+
+#     correct_prediction = tf.equal(tf.argmax(average_y, 1),tf.argmax(y_, 1))
+#     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+#     with tf.Session() as sess:
+#         tf.global_variables_initializer().run()
+#         validate_feed = {x:mnist.validation.images,y_:mnist.validation.labels}
+
+#         test_feed = {x:mnist.test.images, y_:mnist.test.labels}
+
+#         for i in range(TRAINING_STEPS):
+#             if i % 1000 == 0:
+#                 validate_acc = sess.run(accuracy, feed_dict=validate_feed)
+#                 print("After %d training steps,test accuracy using average model is %g" % (i,validate_acc))
+            
+#             xs, ys = mnist.train.next_batch(BATCH_SIZE)
+#             sess.run(train_op, feed_dict={x: xs, y_: ys})
+#         test_acc = sess.run(accuracy, feed_dict=test_feed)
+#         print("After %d training steps,test accuracy using average model is %g" % (TRAINING_STEPS,test_acc))
+# def main(argv = None):
+#     mnist = input_data.read_data_sets("/Users/steven/Desktop/MNIST_DATA",one_hot=True)
+#     train(mnist)
+
+# if __name__ == '__main__':
+#     tf.app.run()
+# # fig = plt.figure()  
+# # plt.imshow(mnist.train.images[10].reshape(28,28),cmap='gray')
+# # plt.show()
